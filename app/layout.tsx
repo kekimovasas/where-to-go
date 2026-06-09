161c8a15310ad6fab887b963ae212b44335f2df4
kekimovasas <k.ekimova.sas@gmail.com>
2026-06-10 00:53:50 +0500

    Initial Where To Go MVP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import { PostHogProvider } from "./providers";
 import "./globals.css";
 
@@ -27,6 +28,38 @@ export default function RootLayout({
         <PostHogProvider>
           {children}
           <Analytics />
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109747201', 'ym');
+
+              ym(109747201, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce: true,
+                trackLinks: true
+              });
+            `}
+          </Script>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/109747201"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          </noscript>
         </PostHogProvider>
       </body>
     </html>
